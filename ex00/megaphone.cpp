86ec7cef18935776d993821760eec2fa4bd53bd1
ywake <ywake@student.42tokyo.jp>
2022-01-27 02:24:21 +0900

    add: ex00 fix #1

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
-int	main()
-{
-	std::cout << "hello" << std::endl;
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+  } else {
+    for (size_t i = 1; i < (size_t)argc; i++) {
+      std::string str = "";
+      for (size_t j = 0; argv[i][j]; j++) {
+        str += std::toupper(argv[i][j]);
+      }
+      std::cout << str;
+    }
+		std::cout << std::endl;
+  }
 }
